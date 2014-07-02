f0268f19d0c79c4e29012743c4e69c521df22652
Chris Sainty <csainty@hotmail.com>
2014-07-02 20:18:47 +0200

    Initial commit of Try Veil hack

---
+++ Content/js/site.js
@@ -0,0 +1,56 @@
+﻿/** @jsx React.DOM */
+
+var TemplateEditor = React.createClass({
+    getInitialState: function() {
+        return {
+            template: "{{AlbumName}} - {{Artist}}\n{{#each Tracks}}\n{{TrackName}} - {{Length}}\n{{/each}}",
+            parser: "handlebars",
+            results: ""
+        };
+    },
+    handleTemplateChange: function(event) {
+        var self = this;
+        this.setState({template: event.target.value }, this.executeTemplate);
+    },
+    handleParserChange: function (event) {
+        this.setState({ parser: event.target.value }, this.executeTemplate);
+    },
+    handleSubmit: function (event) {
+        this.executeTemplate();
+    },
+    executeTemplate: _.debounce(function () {
+        var self = this;
+       
+        $.post("/" + this.state.parser, this.state.template).then(function (data) {
+            self.setState({ results: data });
+        });
+    }, 500),
+    render: function() {
+        return (
+            <div>
+                Choose Parser:
+                <select value={this.state.parser} onChange={this.handleParserChange}>
+                    <option value="handlebars">Veil.Handlebars</option>
+                    <option value="supersimple">Veil.SuperSimple</option>
+                </select>
+                <div className="row">
+                    <div className="col-md-5">
+                        <textarea value={this.state.template} onChange={this.handleTemplateChange}></textarea>
+                    </div>
+                    <div className="col-md-2 middle-column">
+                        <button onClick={this.handleSubmit}>Render</button>
+                    </div>
+                    <div className="col-md-5">
+                        <textarea value={this.state.results}></textarea>
+                    </div>
+               </div>
+            </div>
+        );
+    }
+});
+
+React.renderComponent(
+    <TemplateEditor />,
+    document.getElementById("template-editor")
+);
+
---
+++ TryVeilModule.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Nancy;
+using Newtonsoft.Json;
+using Veil;
+
+namespace TryVeil
+{
+    public class TryVeilModule : NancyModule
+    {
+        private static readonly VeilEngine Engine = new VeilEngine();
+
+        private static readonly ViewModel ViewModel = new ViewModel
+        {
+            AlbumName = "F♯A♯∞",
+            Artist = "Godspeed You! Black Emperor",
+            Tracks = new[] {
+                new TrackViewModel { TrackName = "The Dead Flag Blues", Length = TimeSpan.FromSeconds(16*60+27) },
+                new TrackViewModel { TrackName = "East Hastings", Length = TimeSpan.FromSeconds(17*60+58) },
+                new TrackViewModel { TrackName = "Providence", Length = TimeSpan.FromSeconds(29*60+02) }
+            }
+        };
+
+        private static readonly Lazy<string> ViewModelJson = new Lazy<string>(() =>
+        {
+            return JsonConvert.SerializeObject(ViewModel, Formatting.Indented);
+        });
+
+        public TryVeilModule()
+        {
+            Get["/"] = _ => View["HomePage", ViewModelJson.Value];
+
+            Post["/{parserKey}"] = _ =>
+            {
+                string parserKey = _.parserKey;
+
+                try
+                {
+                    using (var reader = new StreamReader(Request.Body))
+                    {
+                        var template = Engine.Compile<ViewModel>(parserKey, reader);
+
+                        return new Response()
+                        {
+                            StatusCode = HttpStatusCode.OK,
+                            ContentType = "text/html",
+                            Contents = s =>
+                            {
+                                using (var writer = new StreamWriter(s))
+                                {
+                                    template(writer, ViewModel);
+                                }
+                            }
+                        };
+                    }
+                }
+                catch (Exception e)
+                {
+                    return Response.AsText(e.Message);
+                }
+            };
+        }
+    }
+
+    public class ViewModel
+    {
+        public string AlbumName { get; set; }
+
+        public string Artist { get; set; }
+
+        public IEnumerable<TrackViewModel> Tracks { get; set; }
+    }
+
+    public class TrackViewModel
+    {
+        public string TrackName { get; set; }
+
+        public TimeSpan Length { get; set; }
+    }
+}
